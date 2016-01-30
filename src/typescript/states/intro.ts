4bae57f9a41fad6cabe616bf1fb8fa4d6e3deafb
Ricardo Lüders <ricardo@luders.com.br>
2016-01-30 11:35:13 -0200

    Intro

--- src/typescript/states/intro.ts
+++ src/typescript/states/intro.ts
@@ -3,8 +3,75 @@ module PhaserSkeleton.States {
 
     export class Intro extends Phaser.State {
 
+        tweenChain: Array<Phaser.Tween> = [];
+
+        protected createText(text: string, x: number, y: number, style: any) {
+            var _text = this.game.add.text(
+                x, y, text, style
+            );
+            _text.anchor.set(0.5);
+            _text.alpha = 0;
+
+            return _text;
+        }
+
         create() {
-            console.log('Introduction');
+
+            var music = this.game.add.audio('intro');
+            music.play();
+
+            var textStyle1: any = {font: '18px Arial', fill: '#fff', align: 'center'};
+            var textStyle2: any = {font: '20px Arial', fill: '#fff', align: 'center'};
+            var textGroup: string[] = [
+                // Style 1
+                '\"Papai! Papai!? O que aconteceu?\"',
+                '\"Calma, filho. Vai ficar tudo bem...\"',
+                '\"Existem possibilidades, seu filho pode ter uma vida normal.\"',
+                '\"Respire fundo, filho. Tudo vai dar certo, nós somos uma família!\"',
+                // Style 2
+                'As vezes é preciso entender que as coisas mudam...',
+                'Nossas vidas e nossa rotina.',
+                'Porém aquilo que realmente importa, isto permanece...',
+                'Mesmo que em alguns momentos',
+                'Não seja possível compreender...',
+                'Aquilo que nos torna único ficará sempre guardado...',
+                'Em nosso coração.',
+                'É preciso encarar cada novo desafio...',
+                'Cada novo obstáculo...',
+                'E criar um novo ritual...',
+                'Diário',
+                'Constante',
+                'Em busca da felicidade...',
+                'Através da superação.'
+            ];
+
+            for (var i = 0; i < textGroup.length; i++) {
+                var text = this.createText(
+                    textGroup[i],
+                    this.game.world.centerX,
+                    this.game.world.centerY,
+                    (i <= 3) ? textStyle1 : textStyle2
+                );
+
+                var tween = this.game.add.tween(text)
+                                        .to({alpha: 1}, 2000, Phaser.Easing.Linear.None, false)
+                                        .to({alpha: 0}, (i == 3 ? 4000 : 3000), Phaser.Easing.Linear.None, false);
+                if (i == textGroup.length) {
+                    tween.onComplete.add(function() {
+                        console.log('Load level');
+                    }, this);
+                }
+                if (i > 0) {
+                    this.tweenChain[i - 1].chain(tween);
+                }
+                this.tweenChain.push(tween);
+            }
+
+            this.game.time.events.add(2000, this.start, this);
+        }
+
+        start() {
+            this.tweenChain[0].start();
         }
 
     }
--- src/typescript/states/preloader.ts
+++ src/typescript/states/preloader.ts
@@ -1,36 +1,38 @@
 // <reference path="../../../tsDefinitions/phaser.d.ts" />
 module PhaserSkeleton.States {
 
-	export class Preloader extends Phaser.State {
+    export class Preloader extends Phaser.State {
 
-		preloadBar: Phaser.Sprite;
-		background: Phaser.TileSprite;
+        preloadBar: Phaser.Sprite;
+        background: Phaser.TileSprite;
 
-		preload() {
-			this.background = this.add.tileSprite(0, 0, 800, 600, 'preloadBg');
-			this.preloadBar = this.add.sprite(250, 250, 'preloadBar');
-			this.load.setPreloadSprite(this.preloadBar);
+        preload() {
+            this.background = this.add.tileSprite(0, 0, 800, 600, 'preloadBg');
+            this.preloadBar = this.add.sprite(250, 250, 'preloadBar');
+            this.load.setPreloadSprite(this.preloadBar);
 
-			this.loadGameAssets();
-		}
+            this.loadGameAssets();
+        }
 
-		loadGameAssets() {
-			// Load your game loadGameAssets
-			this.load.image('bolt', 'assets/sprites/bolt.png');
-			this.load.image('greenSnail', 'assets/sprites/green-snail.png');
-			this.load.image('player', 'assets/sprites/player.png');
-			this.load.image('redSnail', 'assets/sprites/red-snail.png');
-		}
+        loadGameAssets() {
+            this.load.image('bolt', 'assets/sprites/bolt.png');
+            this.load.image('greenSnail', 'assets/sprites/green-snail.png');
+            this.load.image('player', 'assets/sprites/player.png');
+            this.load.image('redSnail', 'assets/sprites/red-snail.png');
 
-		create() {
-			var tween = this.add.tween(this.preloadBar).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true);
-			tween.onComplete.add(this.startMainMenu, this);
-		}
+            // @todo convert audios to ogg
+            this.load.audio('intro', ['assets/musics/intro.mp3']);
+        }
 
-		startMainMenu() {
-			this.game.state.start('Menu', true, false);
-		}
+        create() {
+            var tween = this.add.tween(this.preloadBar).to({ alpha: 0 }, 1000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startMainMenu, this);
+        }
 
-	}
+        startMainMenu() {
+            this.game.state.start('Menu', true, false);
+        }
+
+    }
 
 }
